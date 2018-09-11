9f5a563647f4fd9904f2e35a075751ff4c2fb528
Patcharapoln <patcharapol.n@ku.th>
2018-09-11 20:44:12 +0700

    Improve Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import picture from '../static/small_logo.jpg'
 import './styles/Nav.css'
-import { Navbar, NavItem, Nav } from 'react-bootstrap'
-
+import {Grid,Col} from 'react-bootstrap'
 
 class NavBar extends Component {
     render() {
         return (
-            <div className = "bar">
-                <a href="#home"><img src = {picture} alt="logo" width='200px'/></a>
-            </div>
+            <Grid class = 'bar'>
+                <Col md={10}> 
+                    <a href="#home"><img src={picture} alt="logo" width='250px' /></a>
+                </Col>
+                <Col md={1}>
+                    <a id='login' href="">Login</a>
+                    <a id='login' href="">Register</a>
+                </Col>
+            </Grid>
         )
     }
 }
