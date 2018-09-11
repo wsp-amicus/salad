bc43fe07d4816b2bc88921473bed2e53b170d876
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-11 11:34:00 +0700

    :checkered_flag: fix windows can't install dependency

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-
 let port = process.env.PORT || 5000
+
+app.get('/test', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    Axios.get('/test').then((res) => console.log(res.data))
+  }
+
   render() {
     return (
       <div className="App">
