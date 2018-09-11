faa95980b519206df698634479b136918df90199
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-11 22:32:56 +0700

    change nav link style, add login page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import picture from '../static/small_logo.jpg'
 import './styles/Nav.css'
-import {Grid,Col} from 'react-bootstrap'
+import { Grid, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
     render() {
+        const currentURL = window.location.pathname
+        console.log(currentURL);
         return (
             <Grid className='bar wrapper'>
                 <Col md={10}> 
-                    <a href="#home"><img src={picture} alt="logo" width='250px' /></a>
+                    <Link to="/"><img src={picture} alt="logo" width='250px' /></Link>
                 </Col>
                 <Col md={1}>
-                    <a id='login' href="">Login</a>
-                    <a id='login' href="">Register</a>
+                    <Link to="/users/login" className={`login ${currentURL === '/users/login' ? 'active':''}`}>Login</Link>
+                    <Link to="/users/register" className={`login ${currentURL === '/users/register' ? 'active':''}`}>Register</Link>
                 </Col>
             </Grid>
         )
