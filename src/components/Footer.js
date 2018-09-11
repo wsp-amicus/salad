192ee5930013f6f618cf9c4f7156ae45af2a66d7
winChawakorn <chawakorn.su@ku.th>
2018-09-11 16:49:09 +0700

    Refactor footer horizontal line

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,37 @@
 import React, { Component } from 'react'
-import {Grid,Row,Col} from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
 import './styles/Footer.css'
 
 
 export default class Home extends Component {
-    box() {
-      return (
-        <Col  md={3}>
-          <div className="outset"></div>
-        </Col>
-      )
-    }
-    render() {
-        return (
-          <div className="container font">    
-          <Grid>
-            <Row className="show-grid">
-              <Col  md={3}>
+  render() {
+    const line = (
+      <Col md={3}>
+        <div className="outset"></div>
+      </Col>
+    )
+    return (
+      <div className="container font">
+        <Grid>
+          <Row className="show-grid">
+            <Col md={3}>
               <p>Amicus</p>
-              </Col>
-              <Col  md={3}>
+            </Col>
+            <Col md={3}>
               <p>Product</p>
-              </Col>
-              <Col md={3}>
-                <p>Useful Link</p>
-              </Col>
-              <Col md={3}>
-                <p>Contact</p>
-              </Col>
-            </Row>
-            <Row >
-              <Col  md={3}>
-                <div className="outset"></div>
-              </Col>
-              <Col  md={3}>
-                <div className="outset"></div>
-              </Col>
-              <Col  md={3}>
-                <div className="outset"></div>
-              </Col>
-              <Col  md={3}>
-                <div className="outset"></div>
-              </Col>
-            </Row>
-          </Grid>
-          </div>
-      )
+            </Col>
+            <Col md={3}>
+              <p>Useful Link</p>
+            </Col>
+            <Col md={3}>
+              <p>Contact</p>
+            </Col>
+          </Row>
+          <Row >
+            {line}{line}{line}{line}
+          </Row>
+        </Grid>
+      </div>
+    )
   }
 }
