985514de0d85de02932907d883067a9a232490e7
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-11 16:17:08 +0700

    add register page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import NotFound from './components/NotFound'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import NavBar from './components/Nav'
+import Register from './components/users/Register'
+import Login from './components/users/Login'
 
 class App extends Component {
   componentDidMount() {
+    // test api
     Axios.get('/test').then((res) => console.log(res.data))
   }
 
@@ -18,6 +21,12 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={Home} />
+
+            {/* Users */}
+            <Route path="/users/register" component={Register} />
+            <Route path="/users/login" component={Login} />
+
+            {/* 404 not found */}
             <Route component={NotFound} />
           </Switch>
         </div>
