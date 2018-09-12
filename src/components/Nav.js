e8bf8ae79bf8cc572c515853d7ab89859d1dcf78
Patcharapoln <patcharapol.n@ku.th>
2018-09-12 15:44:54 +0700

    Improve Nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import picture from '../static/small_logo.jpg'
 import '../styles/Nav.css'
-import { Grid, Col } from 'react-bootstrap'
+import { Grid, Col, ButtonToolbar, MenuItem} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            overflowWidth: 991
+        }
+        console.log(this.props.width);
+        
+    }
+
+    menu() {
+        if (this.props.width < this.state.overflowWidth) {
+            return (<ButtonToolbar>
+                <MenuItem eventKey="1">Action</MenuItem>
+            </ButtonToolbar>)
+        } else {
+
+        }
+    }
+
     render() {
         const currentURL = window.location.pathname
         return (
@@ -15,6 +34,9 @@ class NavBar extends Component {
                 <Col md={1}>
                     <Link to="/users/login" className={`login ${currentURL === '/users/login' ? 'active' : ''}`}>Login</Link>
                     <Link to="/users/register" className={`login ${currentURL === '/users/register' ? 'active' : ''}`}>Register</Link>
+                    {this.props.width <= this.state.overflowWidth ?
+                        <a>true</a>
+                    :<a>false</a>}
                 </Col>
             </Grid>
         )
