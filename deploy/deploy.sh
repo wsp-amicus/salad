5eb20fbaf14b98b0c1e2b18ce2216690673f42e8
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-14 16:57:36 +0700

    update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 USERNAME=wsp
 HOSTS="wsp.thitgorn.com"
-SCRIPT="cd salad;
+SCRIPT="
+cd salad;
 git pull;
 npm install;
+kill \$(lsof -t -i :5000);
+sudo kill \$(sudo lsof -t -i :80);
 npm run build;
-serve -s build;
-npm run backend-prod;"
+sudo serve -l 80 -s build/ & npm run backend-prod;
+echo 'Server is started CTRL + C to leave'"
 ssh -l ${USERNAME} ${HOSTS} "${SCRIPT}"
 
