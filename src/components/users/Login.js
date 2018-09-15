401c0541c8737c365abc8a667f9156d4a218e05b
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-15 22:08:51 +0700

    prepare login

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react'
+import { genSalt, hash } from 'bcryptjs'
 import '../../styles/Login.css'
 
 export class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value })
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    genSalt(10, (err, salt) => {
+        hash(this.state.password, salt, (err, hash) => {
+          // axios login
+          console.log(hash);
+        });
+    });
+  }
+
   render() {
     return (
       <div className='container'>
@@ -10,14 +37,14 @@ export class Login extends Component {
           <form>
             <div className="form-group">
               <label>Username</label>
-              <input type="text" className="form-control" id="username" placeholder="example"  autoComplete="username"/>
+              <input type="text" className="form-control" id="username" placeholder="example" autoComplete="username" value={this.state.username} onChange={this.handleUsernameChange.bind(this)} />
             </div>
 
             <div className="form-group">
               <label>Password</label>
-              <input type="password" className="form-control" id="password" placeholder="......" autoComplete="new-password"/>
+              <input type="password" className="form-control" id="password" placeholder="......" autoComplete="new-password" value={this.state.password} onChange={this.handlePasswordChange.bind(this)} />
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary" onClick={this.handleSubmit.bind(this)}>Submit</button>
           </form>
         </div>
         <hr />
