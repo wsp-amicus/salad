6de6336e16737763ad3d36c3f26b959203e7f8b0
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-16 21:18:27 +0700

    add deploy ssl script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,12 +2,20 @@
 USERNAME=wsp
 HOSTS="wsp.thitgorn.com"
 SCRIPT="
+echo '${USERNAME}: cd salad';
 cd salad;
+echo '${USERNAME}: git pull';
 git pull;
+echo '${USERNAME}: npm install';
 npm install;
+echo '${USERNAME}: kill old process ,sudo kill \$(sudo lsof -t -i :5555)';
 sudo kill \$(sudo lsof -t -i :5555);
+echo '${USERNAME}: building production file'
 npm run build;
-echo 'Server is started CTRL + C to leave';
-PORT=5555 NODE_ENV='production' node server/app.js; \n"
+echo '${USERNAME}: Server is started CTRL + C to leave';
+PORT=5555 NODE_ENV='production' node server/app.js;
+"
+
+echo "you are about to deploy app to ${USERNAME}@${HOSTS}"
 ssh -l ${USERNAME} ${HOSTS} "${SCRIPT}"
 
--- deploy/setup.sh
+++ deploy/setup.sh
@@ -4,3 +4,11 @@ sudo apt update;
 sudo apt install nodejs;
 sudo apt install npm;
 nodejs -v;
+
+# ssl field
+sudo add-apt-repository ppa:certbot/certbot;
+sudo apt-get update;
+sudo apt-get install certbot;
+
+# register ssl certificate
+certbot certonly --manual
