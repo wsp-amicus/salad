6404ce5bbc4345eb138be5d235e415b7553804d1
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-16 21:29:02 +0700

    verify ssl

--- server/app.js
+++ server/app.js
@@ -21,6 +21,11 @@ if(process.env.NODE_ENV === 'production') {
     // serve react app in production
     app.use(express.static(`${__dirname}/../build`))
 
+    // certificate
+    app.get('/.well-known/acme-challenge/5TiT4Mxfc9atVdR6YsVF28fIx1DvwaJJkdesn81_1d8', (req,res) => {
+        res.send('5TiT4Mxfc9atVdR6YsVF28fIx1DvwaJJkdesn81_1d8.Uu7iEQCQf9geWmNlvr7t9ugz1CbwmFs6JNiD4LSq_lE')
+    })
+
     // Handles any requests that don't match the ones above
     app.get('*', (req,res) =>{
         res.sendFile(path.join(__dirname + '/../build/index.html'));
