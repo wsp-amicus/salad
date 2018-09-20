2978b3027e55cbb5b1a5eb75ac456053e78035f3
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-09-21 00:50:14 +0700

    redirect http to https in production

--- server/app.js
+++ server/app.js
@@ -35,6 +35,16 @@ app.use('/users', require('./routes/users'))
 
 // use this in production only
 if(process.env.NODE_ENV === 'production') {
+    // redirect http to https
+    app.use (function (req, res, next) {
+            if (req.secure) {
+                    // request was via https, so do no special handling
+                    next();
+            } else {
+                    // request was via http, so redirect to https
+                    res.redirect('https://' + req.headers.host + req.url);
+            }
+    });
     // serve react app in production
     app.use(express.static(`${__dirname}/../build`))
     
