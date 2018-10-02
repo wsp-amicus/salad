76ec9b40e165d2337bee83caab1493b705294c2a
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-10-02 14:28:28 +0700

    update for https redirection

--- server/app.js
+++ server/app.js
@@ -39,46 +39,43 @@ app.use('/dashboard', require('./routes/dashboard'))
 app.use('/product', require('./routes/product'))
 app.use('/storage', require('./routes/storage'))
 
-app.get('/.well-known/acme-challenge/ADVa8lonkXrae-TJ9hCfmXMX4uye0a5fune9d7xbkyA', (req,res)=> {
-    res.send('ADVa8lonkXrae-TJ9hCfmXMX4uye0a5fune9d7xbkyA.FO5qm4PIP8INnsRscbbh8RjILzEgmTCDmGdmRolKr8o')
-})
 // use this in production only
-// if(process.env.NODE_ENV === 'production') {
-//     // redirect http to https
-//     // app.use (function (req, res, next) {
-//     //         if (req.secure) {
-//     //                 // request was via https, so do no special handling
-//     //                 next();
-//     //         } else {
-//     //                 // request was via http, so redirect to https
-//     //                 res.redirect('https://' + req.headers.host + req.url);
-//     //         }
-//     // });
-//     // serve react app in production
-//     app.use(express.static(`${__dirname}/../build`))
+if(process.env.NODE_ENV === 'production') {
+    // redirect http to https
+    app.use (function (req, res, next) {
+            if (req.secure) {
+                    // request was via https, so do no special handling
+                    next();
+            } else {
+                    // request was via http, so redirect to https
+                    res.redirect('https://' + req.headers.host + req.url);
+            }
+    });
+    // serve react app in production
+    app.use(express.static(`${__dirname}/../build`))
     
-//     // Handles any requests that don't match the ones above
-//     app.get('*', (req,res) =>{
-//         res.sendFile(path.join(__dirname + '/../build/index.html'))
-//     })
+    // Handles any requests that don't match the ones above
+    app.get('*', (req,res) =>{
+        res.sendFile(path.join(__dirname + '/../build/index.html'))
+    })
 
-//     // # certificate part HTTPS
-//     // Certificate
-//     // const privateKey = fs.readFileSync('/etc/letsencrypt/live/wsp.thitgorn.com/privkey.pem', 'utf8')
-//     // const certificate = fs.readFileSync('/etc/letsencrypt/live/wsp.thitgorn.com/cert.pem', 'utf8')
-//     // const ca = fs.readFileSync('/etc/letsencrypt/live/wsp.thitgorn.com/chain.pem', 'utf8')
+    # certificate part HTTPS
+    Certificate
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/wsp.thitgorn.com/privkey.pem', 'utf8')
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/wsp.thitgorn.com/cert.pem', 'utf8')
+    const ca = fs.readFileSync('/etc/letsencrypt/live/wsp.thitgorn.com/chain.pem', 'utf8')
 
-//     // const credentials = {
-//     //     key: privateKey,
-//     //     cert: certificate,
-//     //     ca: ca
-//     // }
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    }
     
-//     // const httpsServer = https.createServer(credentials, app)
-//     // httpsServer.listen(5556, () => {
-//     //     console.log('HTTPS Server running on port 5556')
-//     // })
-// }
+    const httpsServer = https.createServer(credentials, app)
+    httpsServer.listen(5556, () => {
+        console.log('HTTPS Server running on port 5556')
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`)
