6a0f8a748912227b3b2fec68a9cbfe9b7d0123f1
winChawakorn <chawakorn.su@ku.th>
2018-10-31 15:33:45 +0700

    Make bowl and ingredient responsive

--- src/components/Custom.js
+++ src/components/Custom.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import '../styles/Custom.css'
 import { Image } from 'react-bootstrap'
 import bowl from '../static/bowl.png'
+import lettuce from '../static/lettuce.png'
 
 export default class Custom extends Component {
   render() {
     return (
       <div className="custom-container">
-        <div className="bowl">
-          <Image src={bowl} responsive width="700px" />
+        <h1>Create your own salad!</h1>
+        <div className="bowl" id="bowl">
+          <Image className="bowl" src={bowl} responsive width="700px" />
+          <Image className="ingredient" src={lettuce} responsive width="70%" />
         </div>
       </div>
     )
