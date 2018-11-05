3c25b28bd1ccd1ef22c89626bda791cf2c67e36e
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-11-05 14:28:04 +0700

    add basic test

--- src/route.users.test.js
+++ src/route.users.test.js
@@ -1,29 +1,41 @@
-const request = require('supertest')
-const server = require('../../server/app')
+import add from '../server/test'
+const request = require("supertest");
+const server = require("../server/app");
 
-const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+describe("test function add", () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+})
+
+const randomString =
+  Math.random()
+    .toString(36)
+    .substring(2, 15) +
+  Math.random()
+    .toString(36)
+    .substring(2, 15);
 
 const user = {
   firstname: randomString,
   lastname: randomString,
   email: `${randomString}@email.com`,
   username: randomString,
-  password: 'password',
-}
+  password: "password"
+};
 
-describe('POST /users/register', () => {
-  it('should return "You are ready to login!"', function (done) {
-    console.log('here')
+describe("POST /users/register", () => {
+  it('should return "You are ready to login!"', function(done) {
+    console.log("here");
     request(server)
-      .post('/users/register')
-      .set('Content-Type', 'application/json')
+      .post("/users/register")
+      .set("Content-Type", "application/json")
       .send(JSON.stringify(user))
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .expect(200)
-      .expect(function (res) {
-        expect(res).toBe('You are ready to login!')
-      })
-    done()
-  })
-})
-ƒ
+      .expect(function(res) {
+        expect(res).toBe("You are ready to login!");
+      });
+    done();
+  });
+});
