bedeb16223033856d36d892c340b714d9abe4be4
Patcharapoln <patcharapol.n@ku.th>
2018-11-05 22:58:05 +0700

    Use real menu in checkout page

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import { observer } from "mobx-react";
-import { FormGroup, FormControl, Button } from "react-bootstrap";
-import InputAddress from "react-thailand-address-autocomplete";
-import { Store } from "../../store/product";
-import CartImage from "../../static/cart.png";
-import SalmonImage from "../../static/Salmon.jpg";
-import "../../styles/Ingredients.css";
-import "./checkout.css";
-const _store = Store.getInstance();
+import React, { Component } from 'react'
+import { observer } from 'mobx-react'
+import { FormGroup, FormControl, Button } from 'react-bootstrap'
+import InputAddress from 'react-thailand-address-autocomplete'
+import { Store } from '../../store/product'
+import CartImage from '../../static/cart.png'
+import { addProduct2Cart } from '../../store/product'
+import '../../styles/Ingredients.css'
+import './checkout.css'
+import axios from 'axios'
+const _store = Store.getInstance()
 
 class Checkout extends Component {
   constructor(props) {
@@ -17,15 +18,27 @@ class Checkout extends Component {
       district: '',
       province: '',
       zipcode: '',
+      products: []
     }
     this.onChange = this.onChange.bind(this)
     this.onSelect = this.onSelect.bind(this)
+  }
+
+  componentWillMount() {
+    axios
+      .get('/products')
+      .then(res => this.setState({ products: res.data, loading: false }))
+      .catch(err => console.log(err))
   }
 
   onChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     })
+  }
+
+  addProduct2Cart(product) {
+    addProduct2Cart(product)
   }
 
   onSelect(fullAddress) {
@@ -40,7 +53,7 @@ class Checkout extends Component {
 
   getTotal(products) {
     let price = 0
-    products.forEach(product => price += product.price)
+    products.forEach(product => (price += product.price))
     return price
   }
 
@@ -50,108 +63,75 @@ class Checkout extends Component {
         <div
           key={product.name}
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            border: "1px solid black",
-            borderRadius: "5px",
-            padding: "5px 20px",
-            margin: "10px"
+            display: 'flex',
+            justifyContent: 'space-between',
+            border: '1px solid black',
+            borderRadius: '5px',
+            padding: '5px 20px',
+            margin: '10px'
           }}
         >
           <img
             src={product.imageUrl}
-            style={{ width: "100px", height: "100%" }}
+            style={{ width: '100px', height: '100%' }}
             alt="product"
           />
-          <h4 style={{ marginTop: "25px" }}>{product.name}</h4>
-          <h4 style={{ marginTop: "25px" }}>{product.price} ฿</h4>
+          <h4 style={{ marginTop: '25px' }}>{product.name}</h4>
+          <h4 style={{ marginTop: '25px' }}>{product.price} ฿</h4>
         </div>
-      );
-    });
+      )
+    })
   }
 
-  getMockSuggestion(lengths) {
-    return lengths.map(l => {
-      return (
-        <div className="box-container" style={{ width: "200px" }}>
-          <div className="image-container">
-            <img
-              className="fadeIn"
-              id="box"
-              src={SalmonImage}
-              alt="ingredients"
-              height="auto"
-              width="auto"
-            />
-            <div className="button-container">
-              <Button id="add-button" bsStyle="success">
-                <img id="cart" src={CartImage} alt="cart" />
-                Add
-              </Button>
-            </div>
-          </div>
-          <hr />
-          <h3>Salmon</h3>
-          <h4>Product</h4>
-          <p>Salmon za 555+</p>
-          <h4>99999 ฿</h4>
-        </div>
-      );
-    });
-  }
   render() {
+    const _products = this.state.products.splice(0, 3)
     return (
       <div>
-        <div style={{ textAlign: "center", marginTop: '130px' }}>
-          <h1>Would you like anything else?</h1>
-
-          <div style={{ display: "flex", width: "80%", margin: "auto" }}>
-            {this.getMockSuggestion([{}, {}, {}])}
-          </div>
-        </div>
         <div
           style={{
-            margin: "20px",
-            padding: "20px",
-            border: "1px solid black",
-            borderRadius: "25px"
+            margin: '20px',
+            padding: '20px',
+            border: '1px solid black',
+            borderRadius: '25px'
           }}
         >
           <h2>Order Review</h2>
-
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              border: "1px solid black",
-              padding: "0px 20px",
-              margin: "10px",
-              background: "rgba(0,0,0,0.8)",
-              color: "white"
+              display: 'flex',
+              justifyContent: 'space-between',
+              border: '1px solid black',
+              padding: '0px 20px',
+              margin: '10px',
+              background: 'rgba(0,0,0,0.8)',
+              color: 'white'
             }}
           >
             <h4>Image</h4>
             <h4>Name</h4>
             <h4>Price</h4>
           </div>
-          {this.getListComponent([..._store.products])}
-          <div style={{ textAlign: "right" }}>
+          {this.getListComponent(_store.products)}
+          <div style={{ textAlign: 'right' }}>
             <h3>Total</h3>
             <h3>{this.getTotal(_store.products)} ฿</h3>
           </div>
         </div>
         <div
           style={{
-            margin: "20px",
-            padding: "20px",
-            border: "1px solid black",
-            borderRadius: "25px"
+            margin: '20px',
+            padding: '20px',
+            border: '1px solid black',
+            borderRadius: '25px'
           }}
         >
           <h2>Derivery address</h2>
           <label>ที่อยู่</label>
           <FormGroup bsSize="large" className="address">
-            <FormControl type="text" style={{ height: '37px', color: 'black' }} />
+            <FormControl
+              type="text"
+              style={{ height: '37px', color: 'black' }}
+            />
             <label>แขวง/ตำบล</label>
             <InputAddress
               address="subdistrict"
@@ -182,14 +162,46 @@ class Checkout extends Component {
             />
           </FormGroup>
         </div>
-        <div style={{ textAlign: "center", margin: "40px" }}>
-          <Button bsStyle="primary" bsSize="large" style={{ width: "200px" }}>
+        <div style={{ textAlign: 'center', margin: '40px' }}>
+          <Button bsStyle="primary" bsSize="large" style={{ width: '200px' }}>
             Order
           </Button>
         </div>
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <h1>Would you like anything else?</h1>
+          <div style={{ display: 'flex', width: '80%', margin: 'auto' }}>
+            {_products.map(product => (
+              <div className="box-container" style={{ width: '250px' }}>
+                <div className="image-container">
+                  <img
+                    className="fadeIn"
+                    id="box"
+                    src={product.imageUrl[0]}
+                    alt="ingredients"
+                    height="auto"
+                    width="auto"
+                  />
+                  <div className="button-container">
+                    <Button
+                      id="add-button"
+                      bsStyle="success"
+                      onClick={() => this.addProduct2Cart(product)}
+                    >
+                      <img id="cart" src={CartImage} alt="cart" />
+                      Add
+                    </Button>
+                  </div>
+                </div>
+                <hr />
+                <h3>{product.name}</h3>
+                <h4>{product.price} ฿</h4>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default observer(Checkout);
+export default observer(Checkout)
