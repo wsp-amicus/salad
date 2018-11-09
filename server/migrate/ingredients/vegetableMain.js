ebe55948a41ef74ff9d03853503e58b9ff4f715b
Jirayu Laungwilawan <jirayu.l@ku.th>
2018-11-09 16:28:59 +0700

    Add ingredients

---
+++ server/migrate/ingredients/vegetableMain.js
@@ -0,0 +1,31 @@
+const vegetableMain = [{
+    name: 'Green oak',
+    price: 10,
+    type: 'Vegetable (Main)',
+    description: 'Fresh organic green oak lettuce',
+    imageUrl: 'https://cdn.shopify.com/s/files/1/0206/9470/products/Living_Lettuce-green-oak_1024x1024.JPG?v=1503108940'
+  },
+  {
+    name: 'Red oak',
+    price: 10,
+    type: 'Vegetable (Main)',
+    description: 'Fresh organic red oak lettuce',
+    imageUrl: 'https://www.coopathome.ch/img/produkte/880_880/RGB/3633411_001.jpg?_=1457546943675'
+  },
+  {
+    name: 'Green coral',
+    price: 10,
+    type: 'Vegetable (Main)',
+    description: 'Fresh organic green coral lettuce',
+    imageUrl: 'https://www.kingfreshfarm.com/wp-content/uploads/2017/09/Leaf-Lettuce-7.jpg'
+  },
+  {
+    name: 'Red coral',
+    price: 10,
+    type: 'Vegetable (Main)',
+    description: 'Fresh organic red coral lettuce',
+    imageUrl: 'https://fp.lnwfile.com/_/fp/_raw/59/48/z2.jpg'
+  }
+]
+
+export default vegetableMain
