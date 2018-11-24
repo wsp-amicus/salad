b373c4a8eec1b5cbca583aa375daebb383f784a7
winChawakorn <chawakorn.su@ku.th>
2018-11-24 16:24:00 +0700

    Fix navbar bug opening when click on the link

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -36,16 +36,16 @@ class NavBar extends Component {
   getNavLink(to, label, linkColor) {
     return (
       <div>
-        <Link className={`${linkColor}`} to={to} onClick={() => this.expand()}>
+        <Link className={`${linkColor}`} to={to} onClick={() => this.expand(false)}>
           {label}
         </Link>
       </div>
     );
   }
 
-  expand() {
+  expand(isExpanded) {
     if (this.props.width < 768)
-      this.setState({ expanded: !this.state.expanded });
+      this.setState({ expanded: isExpanded || !this.state.expanded });
   }
 
   render() {
@@ -53,21 +53,21 @@ class NavBar extends Component {
     const { transparent } = this.state;
     const barColor = `${
       transparent && currentURL === "/" ? "transparent" : "non-transparent"
-    }`;
+      }`;
     const linkColor = `${transparent && currentURL === "/" ? "white" : ""}`;
     return (
-      <div className="bar wrapper" onClick={() => this.expand()}>
+      <div className="bar wrapper">
         <Navbar
           inverse
           expanded={this.state.expanded}
-          onToggle={() => {}}
+          onToggle={() => { }}
           className={barColor}
         >
           <Navbar.Header>
             <Link to="/">
               <img src={transparent_logo} alt="logo" width="200px" />
             </Link>
-            <Navbar.Toggle />
+            <Navbar.Toggle onClick={() => this.expand()} />
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
@@ -80,46 +80,46 @@ class NavBar extends Component {
                 {this.getNavLink("/users/register", "Register", linkColor)}
               </Nav>
             ) : (
-              <Nav className="navbar-right">
-                <Cart transparent={transparent} />
-                <DropdownButton
-                  title={
-                    <div>
-                      <Glyphicon glyph="user" style={{ marginRight: "10px" }} />
-                      {this.props.user.username}
-                    </div>
-                  }
-                  id="0"
-                  noCaret
-                  open={this.state.userOpen}
-                  onToggle={() => {}}
-                  onMouseOver={() => this.setState({ userOpen: true })}
-                  onMouseLeave={() => this.setState({ userOpen: false })}
-                  className="dropdown-button"
-                >
-                  <li
-                    role="presentation"
-                    style={{
-                      background: transparent ? "transparent" : "#222"
-                    }}
+                <Nav className="navbar-right">
+                  <Cart transparent={transparent} />
+                  <DropdownButton
+                    title={
+                      <div>
+                        <Glyphicon glyph="user" style={{ marginRight: "10px" }} />
+                        {this.props.user.username}
+                      </div>
+                    }
+                    id="0"
+                    noCaret
+                    open={this.state.userOpen}
+                    onToggle={() => { }}
+                    onMouseOver={() => this.setState({ userOpen: true })}
+                    onMouseLeave={() => this.setState({ userOpen: false })}
+                    className="dropdown-button"
                   >
-                    <Link role="menuitem" tabIndex="-1" to="/users/manage">
-                      Manage
+                    <li
+                      role="presentation"
+                      style={{
+                        background: transparent ? "transparent" : "#222"
+                      }}
+                    >
+                      <Link role="menuitem" tabIndex="-1" to="/users/manage">
+                        Manage
                     </Link>
-                  </li>
-                  <li
-                    role="presentation"
-                    style={{
-                      background: transparent ? "transparent" : "#222"
-                    }}
-                  >
-                    <Link role="menuitem" tabIndex="-1" to="/users/logout">
-                      Logout
+                    </li>
+                    <li
+                      role="presentation"
+                      style={{
+                        background: transparent ? "transparent" : "#222"
+                      }}
+                    >
+                      <Link role="menuitem" tabIndex="-1" to="/users/logout">
+                        Logout
                     </Link>
-                  </li>
-                </DropdownButton>
-              </Nav>
-            )}
+                    </li>
+                  </DropdownButton>
+                </Nav>
+              )}
           </Navbar.Collapse>
         </Navbar>
         <style>{`
