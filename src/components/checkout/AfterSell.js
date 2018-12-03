bfdde61c1e7d57882c9aeec1c3474908b6f1ff83
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-12-03 15:33:50 +0700

    Add aftersell page

---
+++ src/components/checkout/AfterSell.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class AfterSell extends Component {
+  render() {
+    return (
+      <div>
+        You order is completed.
+
+        Tracking number is xxx-xxx-xxxxx
+      </div>
+    )
+  }
+}
+
+export default AfterSell
