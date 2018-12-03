9d49724b6283b920646ae31262c6f2899968f62c
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-12-03 19:47:08 +0700

    init transactions admin

---
+++ src/admin/pages/Transaction/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export class Transaction extends Component {
+  render() {
+    return (
+      <div>
+        <div className="panel panel-primary">
+          <div className="panel-heading">
+            <span>Transaction</span>
+          </div>
+          <div className="panel-body">graph</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Transaction;
